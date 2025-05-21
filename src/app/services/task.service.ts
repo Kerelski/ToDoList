e01a4156fa1ad5c21f713e0cc20ddfb7a3fcedb6
Kerelski <karoladamski22@wp.pl>
2025-05-22 00:18:15 +0200

    service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+export type Status = 'Pending' | 'Completed' | 'In Progress';
+
+export interface TaskData{
+  title: string;
+  date: Date;
+  status: Status;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+  private tasksSubject: BehaviorSubject<TaskData[]> = new BehaviorSubject<TaskData[]>([]);
+  tasks$: Observable<TaskData[]> = this.tasksSubject.asObservable();
+
+  private selectedTaskSubject = new BehaviorSubject<TaskData | null>(null);
+  selectedTask$ = this.selectedTaskSubject.asObservable();
+
+  sortByDateAsc = false;
+
+  get tasks(): TaskData[] {
+    return this.tasksSubject.value;
+  }
+
+  get selectedTask(): TaskData | null {
+    return this.selectedTaskSubject.value;
+  }
+
+  setTasks(tasks: TaskData[]): void {
+    this.tasksSubject.next(tasks);
+  }
+
+  addTask(task: TaskData): void {
+    this.tasksSubject.next([...this.tasks, task]);
+  }
+
+  removeTask(task: TaskData): void {
+    this.setTasks(this.tasks.filter((t: TaskData) => t !== task));
+  }
+
+  updateTask(task: TaskData, status: Status): void {
+    const updated: TaskData[] = this.tasks.map((t: TaskData) =>
+      t === task ? {...t, status, date: new Date()} : t
+    );
+    this.setTasks(updated);
+  }
+
+
+  sortByDate(): void {
+    this.sortByDateAsc = !this.sortByDateAsc;
+    const tasks = this.tasksSubject.getValue();
+    const sortedTasks = [...tasks].sort((a, b) => {
+      return this.sortByDateAsc
+        ? a.date.getTime() - b.date.getTime()
+        : b.date.getTime() - a.date.getTime();
+    });
+    this.tasksSubject.next(sortedTasks);
+  }
+
+  selectTask(task: TaskData | null) {
+    this.selectedTaskSubject.next(task);
+  }
+}
